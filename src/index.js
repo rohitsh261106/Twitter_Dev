e322439dae737679226e3ca02b8d5b465b3aa69b
Rohit Sharma <rohit.sh2630@gmail.com>
2026-07-15 10:35:46 +0530

    starting adding create service for tweets

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const connect = require('./config/database');
 const app = express();
-
-const TweetRepository = require('./repository/tweet-repository');
-
-const Comment = require('./models/comment');
+const Tweet = require('./models/tweet')
 
 app.listen(3000,async()=>{
   console.log("Server Started on Port");
   await connect();
   console.log("mongodb connected")
-  // const tweet = await Tweet.create({
-  //   content: 'First Tweet',
-  //   userEmail: 'rohit.sh2611@gmail.com'
-  // });
-  const tweetRepo = new TweetRepository();
-  const tweet = await tweetRepo.getAll(2,4);
-   console.log(tweet[0].contentWithEmail);
+  const tweets = await Tweet.find({
+    content: ['First tweet','my tweet','1234456']
+
+    
+  })
+  console.log(tweets);
+  
 })
