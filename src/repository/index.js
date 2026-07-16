bd8f4c0a302e0ef16535febde14f27610623976c
Rohit Sharma <rohit.sh2630@gmail.com>
2026-07-16 13:01:58 +0530

    Added get tweet with nested comments api

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,9 +2,11 @@ import TweetRepository from './tweet-repository.js'
 import HashtagRepository from './hashtag-repository.js'
 import LikeRespository from './like-repository.js'
 import UserRepository from './user-repository.js'
+import CommentRepository from './comment-repository.js'
 export {
     TweetRepository,
     HashtagRepository,
     LikeRespository,
-    UserRepository
+    UserRepository,
+    CommentRepository
 }
