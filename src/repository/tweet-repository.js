8e9901c562d57823ac4e1432b12bf3832955e258
Rohit Sharma <rohit.sh2630@gmail.com>
2026-07-16 10:26:19 +0530

    Added basic crud repo and extended it in likerepo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,84 +1,50 @@
-import Tweet from '../models/tweet.js';
+import Tweet from '../models/tweet.js'
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
-
+class TweetRepository extends CrudRepository {
+    constructor() {
+        super(Tweet);
+    }
+    
     async create(data) {
         try {
             const tweet = await Tweet.create(data);
             return tweet;
         } catch (error) {
-            throw error;
-        }
-    }
-
-    async get(id) {
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet;
-        } catch (error) {
+            // console.log(error);
             throw error;
         }
     }
 
     async getWithComments(id) {
         try {
-            const tweet = await Tweet.findById(id)
-                .populate({
-                    path: 'comments',
-                    populate: {
-                        path: 'comments'
-                    }
-                })
-                .lean();
-
+            const tweet = await Tweet.findById(id).populate({
+                path: 'comments',
+                populate: {
+                    path: 'comments'
+                }
+            }).lean();
             return tweet;
         } catch (error) {
-            throw error;
+            console.log(error);
         }
     }
 
-    async getAll(offset = 0, limit = 10) {
+    async getAll(offset, limit) {
         try {
-            const tweets = await Tweet.find()
-                .skip(offset)
-                .limit(limit);
-
-            return tweets;
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    async update(id, data) {
-        try {
-            const tweet = await Tweet.findByIdAndUpdate(id, data, {
-                new: true
-            });
-
+            const tweet = await Tweet.find().skip(offset).limit(limit);
             return tweet;
         } catch (error) {
-            throw error;
-        }
-    }
-
-    async destroy(id) {
-        try {
-            const tweet = await Tweet.findByIdAndDelete(id);
-            return tweet;
-        } catch (error) {
-            throw error;
+            console.log(error);
         }
     }
 
     async find(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({
-                path: 'likes'
-            });
-
+            const tweet = await Tweet.findById(id).populate({path: 'likes'});
             return tweet;
         } catch (error) {
-            throw error;
+            console.log(error);
         }
     }
 }
