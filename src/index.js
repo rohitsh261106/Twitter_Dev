98873a55159ce0ea3cde723bd12c8a445c130e0e
Rohit Sharma <rohit.sh2630@gmail.com>
2026-07-17 11:18:25 +0530

    Added passport logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import passport from 'passport';
 import {connect} from './config/database.js';
+import { passportAuth } from './config/jwt-middleware.js';
+
 import apiRoutes from './routes/index.js';
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.use('/api',apiRoutes)
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -20,6 +22,16 @@ userSchema.pre('save', function () {
     const salt = bcrypt.genSaltSync(9);
     this.password = bcrypt.hashSync(this.password, salt);
 });
+
+userSchema.methods.comparePassword = function compare(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+userSchema.methods.genJWT = function generate() {
+    return jwt.sign({id: this._id, email: this.email}, 'twitter_secret', {
+        expiresIn: '1h'
+    });
+}
 
 
 const User = mongoose.model('User', userSchema);
